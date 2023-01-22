5993c8548d8b9c16b1aa89f72300bf114496fff1
Varnaship <kriskoplay05@gmail.com>
2023-01-22 15:52:58 +0200

    I updated the game
    I added some colors and the game to be played best of  5 and edited the invalid inputs to not count as a game

--- C# Fundamentals/Projects/RockPaperScissorsByPetrov/RockPaperScissors.cs
+++ C# Fundamentals/Projects/RockPaperScissorsByPetrov/RockPaperScissors.cs
@@ -1,52 +1,113 @@
 ﻿const string Rock = "Rock";
 const string Paper = "Paper";
 const string Scissors = "Scissors";
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("Choose [r]ock, [p]aper, [s]cissors: ");
-string playerMove = Console.ReadLine();
-Random random = new Random();
-int pcRandomNumber = random.Next(1,4);
+Console.WriteLine("The winner will be picked after best of 5");
+Console.ResetColor();
 string pcMove = "";
-switch(pcRandomNumber)
+int pcWins = 0;
+int playerWins = 0;
+int playedGames = 1;
+bool correctInput = true;
+while (playedGames <=5)
 {
-    case 1:
-        pcMove= Rock;
-        break;
-    case 2:
-        pcMove= Paper;
-        break;
-    case 3:
-        pcMove = Scissors;
-        break;
+    if (correctInput = true)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"This is game number {playedGames} ");
+        Console.ResetColor();
+    }
+
+    string playerMove = Console.ReadLine();
+    Random random = new Random();
+    int pcRandomNumber = random.Next(1, 4);
+    switch (pcRandomNumber)
+    {
+        case 1:
+            pcMove = Rock;
+            break;
+        case 2:
+            pcMove = Paper;
+            break;
+        case 3:
+            pcMove = Scissors;
+            break;
+    }
+    if (playerMove == "r" || playerMove == "rock")
+    {
+        playerMove = Rock;
+    }
+    else if (playerMove == "p" || playerMove == "paper")
+    {
+        playerMove = Paper;
+    }
+    else if (playerMove == "s" || playerMove == "scissors")
+    {
+        playerMove = Scissors;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid input. Try again...");
+        correctInput = false;
+        Console.ResetColor();
+    }
+    if (correctInput == true)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"The computer choose {pcMove}.");
+        Console.ResetColor();
+        correctInput = true;
+        if ((playerMove == Rock && pcMove == Scissors) ||
+        (playerMove == Paper && pcMove == Rock) ||
+        (playerMove == Scissors && pcMove == Paper))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("You win this round. ");
+            Console.ResetColor();
+            playerWins++;
+            correctInput = true;
+        }
+        else if (playerMove == pcMove)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("This round was a draw...");
+            Console.ResetColor();
+            correctInput = true;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You lost this round.");
+            Console.ResetColor();
+            pcWins++;
+            correctInput = true;
+        }
+        playedGames++;
+        if (playedGames <= 5)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Choose [r]ock, [p]aper, [s]cissors: ");
+            Console.ResetColor();
+        }
+    }
 }
-if (playerMove == "r" || playerMove == "rock") 
+if (pcWins>playerWins)
 {
-    playerMove = Rock;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("The computer won the match :(");
+    Console.ResetColor();
 }
-else if (playerMove == "p" || playerMove == "paper") 
+else if(playerWins>pcWins)
 {
-    playerMove = Paper;
-}
-else if (playerMove == "s" || playerMove == "scissors")
-{
-    playerMove = Scissors;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("You won the match!!!");
+    Console.ResetColor();
 }
 else
 {
-    Console.WriteLine("Invalid input. Try again...");
-    return;
-}
-Console.WriteLine($"The computer chose {pcMove}.");
-if ((playerMove == Rock && pcMove == Scissors)||
-    (playerMove == Paper && pcMove == Rock)||
-    (playerMove == Scissors && pcMove == Paper))
-{
-    Console.WriteLine("You win!");
-}
-else if (playerMove == pcMove)
-{
-    Console.WriteLine("This game was a draw...");
-}
-else
-{
-    Console.WriteLine("You lose.");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("The battle was so balanced that there is no winner.... ");
+    Console.ResetColor();
 }
